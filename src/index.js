5fbfa53948704d2b771cf955f01417ac501981ac
Pragesh-Yadav <pragesh.scert@gmail.com>
2022-06-04 12:51:49 +0530

    MongoDB day-0 GET and Post

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,29 +1,53 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
+const bookSchema = new mongoose.Schema( {
+    bookName: {
+        type: String,
+        required: true
+     }, // Book name should be same but Auther name is different.
+    autherName:{
         type: String,
         unique: true,
         required: true
     },
-    emailId: String,
-    gender: {
+    category: {
         type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
+        enum: ["Eduction", "Action", "Classics", "Mystery", "Horror","Drama"]
     },
-    age: Number,
-    // isIndian: Boolean,
-    // parentsInfo: {
-    //     motherName: String,
-    //     fatherName: String,
-    //     siblingName: String
-    // },
-    // cars: [ String  ]
-}, { timestamps: true });
+    year: {
+        type: Number,
+        required: true,
+        unique: true
+    }
+})
 
-module.exports = mongoose.model('User', userSchema) //users
+module.exports = mongoose.model('Book', bookSchema) //books
+
+
+// const userSchema = new mongoose.Schema( {
+//     firstName: String,
+//     lastName: String,
+//     mobile: {
+//         type: String,
+//         unique: true,
+//         required: true
+//     },
+//     emailId: String,
+//     gender: {
+//         type: String,
+//         enum: ["male", "female", "LGBTQ"] //"falana" will give an error
+//     },
+//     age: Number,
+//     // isIndian: Boolean,
+//     // parentsInfo: {
+//     //     motherName: String,
+//     //     fatherName: String,
+//     //     siblingName: String
+//     // },
+//     // cars: [ String  ]
+// }, { timestamps: true });
+
+// module.exports = mongoose.model('User', userSchema) //users
 
 
 
