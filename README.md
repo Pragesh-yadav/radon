70a6978f05981a2b886400296fcad15ae47f7e16
Pragesh-Yadav <pragesh.scert@gmail.com>
2022-06-07 07:31:43 +0530

    Updated

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Creating Book Schema with book name, auther, category and name.
+Creating Book Schema with book name, auther, category and year.
 Creating two API's 
 1. POST
 2. GET
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,13 +1,13 @@
-const UserModel= require("../models/userModel")
+const BookModel= require("../models/userModel")
 
 const createBook= async function (req, res) {
     let data= req.body
-    let savedData= await UserModel.create(data)
+    let savedData= await BookModel.create(data)
     res.send({msg: savedData})
 }
 
 const getAllBooks= async function (req, res) {
-    let allBooks= await UserModel.find()
+    let allBooks= await BookModel.find()
     res.send({msg: allBooks})
 }
 
